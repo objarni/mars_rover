766183734058f551edd60388e9e55ea5f6b79770
Rafael Lawisch <lawisch.nh@gmail.com>
2021-10-10 10:12:58 -0300

    add Rover typing and functions

--- mars_rover.py
+++ mars_rover.py
@@ -1,29 +1,51 @@
+from typing import NamedTuple
+
+
+class Rover(NamedTuple):
+    x: int
+    y: int
+    direction: str
+
+
 directions = ("N", "E", "S", "W")
 
 
 def execute_mission(plateau_size: tuple[int, int], starting_positions: list[tuple[int, int, str]], command_sequences: list[str]) -> list[tuple[int, int, str]]:
     ending_positions = []
     for (starting_position, command_sequence) in zip(starting_positions, command_sequences):
-        rover = {
-            "x": starting_position[0], "y": starting_position[1], "direction": starting_position[2]}
+        rover = Rover(
+            starting_position[0], starting_position[1], starting_position[2])
 
         for command in command_sequence.upper():
-            if command == 'L':
-                rover["direction"] = directions[(
-                    directions.index(rover["direction"]) + 3) % 4]
-            elif command == 'R':
-                rover["direction"] = directions[(
-                    directions.index(rover["direction"]) + 5) % 4]
+            if command in 'LR':
+                rover = turn_rover(rover, command)
             else:
-                if rover["direction"] == "N":
-                    rover["y"] += 1
-                elif rover["direction"] == "E":
-                    rover["x"] += 1
-                elif rover["direction"] == "S":
-                    rover["y"] -= 1
-                elif rover["direction"] == "W":
-                    rover["x"] -= 1
+                rover = move_rover(rover)
 
-        ending_positions.append((rover["x"], rover["y"], rover["direction"]))
+        ending_positions.append(tuple(rover))
 
     return ending_positions
+
+
+def turn_rover(rover: Rover, turn_to: str) -> Rover:
+    new_direction = directions[(
+        directions.index(rover.direction) + 4
+        + (1 if turn_to == 'R' else -1)
+    ) % 4]
+
+    return Rover(rover.x, rover.y, new_direction)
+
+
+def move_rover(rover: Rover) -> Rover:
+    x, y = rover.x, rover.y
+
+    if rover.direction == "N":
+        y += 1
+    elif rover.direction == "E":
+        x += 1
+    elif rover.direction == "S":
+        y -= 1
+    elif rover.direction == "W":
+        x -= 1
+
+    return Rover(x, y, rover.direction)
